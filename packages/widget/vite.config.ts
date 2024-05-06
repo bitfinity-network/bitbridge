af7499870365a77d034dc8b1132f1f218ab79bda
leleking <simeonnortey@gmail.com>
2024-05-06 17:32:57 +0000

    tested widget

--- packages/widget/vite.config.ts
+++ packages/widget/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/index.ts"),
+      name: "widget",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "React-dom",
+        },
+      },
+    },
+  },
+});
