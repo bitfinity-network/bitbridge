5e3711e70520365779b38bc2a5f6091cef11db7b
leleking <simeonnortey@gmail.com>
2024-05-08 20:39:52 +0000

    updated bridge

---
+++ packages/widget/src/utils/ic.ts
@@ -0,0 +1,2 @@
+import { createAgent } from "@bitfinity-network/bridge";
+export const agent = createAgent({ host: "http://127.0.0.1:4943" });
--- packages/widget/src/utils/token.ts
+++ packages/widget/src/utils/token.ts
@@ -1,9 +1,11 @@
 import { TokenList } from "@infinityswapofficial/token-lists";
 import { TokenProp, TokenSearchProps } from "../types";
-import { IcConnector, Icrc1IDL, Icrc1Service } from "@bitfinity/ic";
+import { createICRC1Actor } from "@bitfinity-network/bridge";
 import { NETWORK_SYMBOLS } from "./constants";
 import TokenContractABI from "./abi/erc20.json";
 import { ethers } from "ethers";
+import { Principal } from "@dfinity/principal";
+import { agent } from "./ic";
 
 export const getIcTokens = async (cachedTokens: TokenProp[] = []) => {
   const tokenList = await TokenList.create();
@@ -24,8 +26,9 @@ export const getIcTokens = async (cachedTokens: TokenProp[] = []) => {
 
 export const searchIcrcToken = async (tokenPrincipal: string) => {
   try {
-    const Ic = new IcConnector();
-    const tokenActor = Ic.actor<Icrc1Service>(tokenPrincipal, Icrc1IDL);
+    const tokenActor = createICRC1Actor(Principal.fromText(tokenPrincipal), {
+      agent,
+    });
     const metadata = await Promise.all([
       tokenActor.icrc1_name(),
       tokenActor.icrc1_symbol(),
