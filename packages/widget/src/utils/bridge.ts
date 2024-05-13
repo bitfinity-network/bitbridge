4bb3b8baceaaa9a883a9e3244efe87026295c291
leleking <simeonnortey@gmail.com>
2024-05-13 10:48:17 +0000

    updated bridge utils

--- packages/widget/src/utils/bridge.ts
+++ packages/widget/src/utils/bridge.ts
@@ -11,3 +11,17 @@ export const getEvmWallet = async () => {
     console.log("eth", error);
   }
 };
+
+export const toDecimal = (
+  amount: bigint | number | string,
+  decimal: bigint | number
+) => {
+  return Number(amount) / 10 ** Number(decimal ?? 0);
+};
+
+export const fromDecimal = (
+  amount: bigint | number | string,
+  decimal: bigint | number
+) => {
+  return Math.round(Number(amount) * 10 ** Number(decimal ?? 0));
+};
