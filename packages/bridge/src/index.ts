b00ac6b5ec88718c33092209a6f78194bb9b7f80
leleking <simeonnortey@gmail.com>
2024-05-20 12:25:42 +0000

    updated bridge

--- packages/bridge/src/index.ts
+++ packages/bridge/src/index.ts
@@ -1,12 +1,9 @@
-import 'dotenv/config';
-
 import { BtcBridge } from './btc';
 import { IcrcBridge } from './icrc';
 import { RuneBridge } from './rune';
 import { BTC_TOKEN_WRAPPED_ADDRESS } from './constants';
 
 export { BtcBridge, IcrcBridge, RuneBridge, BTC_TOKEN_WRAPPED_ADDRESS };
-
 export {
   Connector,
   BridgeConfig,
@@ -14,3 +11,5 @@ export {
   Bridges,
   BridgeNetwork
 } from './connector';
+export * from './ic';
+export * from './utils';
