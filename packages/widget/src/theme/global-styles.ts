bfd5481fac6f36da89bd9cac09f5b0d2ed5a01f2
assetcorp <sondelalimic@gmail.com>
2024-06-05 07:59:22 +0000

    fix: global styles

--- packages/widget/src/theme/global-styles.ts
+++ packages/widget/src/theme/global-styles.ts
@@ -1,12 +1,11 @@
-import { fontConfig } from "./font-config";
-import { themeColors } from "./theme-colors";
-import { mode } from "@chakra-ui/theme-tools";
+import { fontConfig } from './font-config';
+import { themeColors } from './theme-colors';
 
 export const globalStyles = {
   body: {
-    bg: mode(themeColors.light.bg.main, themeColors.dark.bg.main),
+    bg: themeColors.bg.main,
     fontFamily: fontConfig.body,
-    color: mode(themeColors.light.text.primary, themeColors.dark.text.primary),
-    top: "0px !important",
-  },
+    color: themeColors.text.primary,
+    top: '0px !important'
+  }
 };
